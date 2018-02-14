95af1e2334c8ce6b25afd615d2d53bd12b4a3782
John Espenhahn <johnshub99@gmail.com>
2018-02-13 22:40:30 -0500

    (Monster class) cleanup whitespace, remove self-explanatory comments, switch style of comments to JavaDoc

--- HelloWorld/src/Monster.java
+++ HelloWorld/src/Monster.java
@@ -1,42 +1,56 @@
-//Represents the computer enemy the Player will face in the game
 public class Monster {
-	//instance variables
 	int health;
 	int maxHealth;
 	int level;
 	int minDamage;
 	int maxDamage;
-	//Constructor, sets the level, min/max damage, and health
+
+	/**
+	 * sets health and damage values based on given level
+	 * @param level The level of this monster
+	 */
 	public Monster(int level) {
 		this.level = level;
 		this.setHealth();
 		this.setDamage();
 	}
-	//this method reduces the health of the monster according to @param damage
+
 	public void damage(int damage) {
 		this.health -= damage;
 		System.out.println("Monster took " + damage + " damage!");
 	}
-	//this sets the max health and heals the monster to full health
+
+	/** (this is a JavaDoc comment)
+	 * this sets the max health and heals the monster to full health
+	 */
 	public void setHealth() {
-		maxHealth = health = 90 + (10*level);
+		maxHealth = health = 90 + (10 * level);
 		health = maxHealth;
 	}
-	//this returns a boolean of whether the monster is dead or not
+
 	public boolean isDead() {
 		boolean dead = health <= 0;
 		return dead;
 	}
-	//this sets the min/max damage values
+
+	/**
+	 * sets the min/max damage values based on the level
+	 */
 	public void setDamage() {
-		minDamage = 10 + level*3;
-		maxDamage = 10 + level*6;
+		minDamage = 10 + level * 3;
+		maxDamage = 10 + level * 6;
 	}
-	//this attacks @param target for @param damage
+
+	/**
+	 * damages target for damage amount
+	 * @param damage The amount of damage to do
+	 * @param target The target
+	 */
 	public void attack(int damage, Player target) {
 		target.damage(damage);
 	}
-	//returns a string representation of the object
+
+	@Override
 	public String toString() {
 		String str = "Monster Level " + level + "\n";
 		str += "Health: " + health + "/" + maxHealth;
